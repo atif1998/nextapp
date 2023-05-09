9b76359374940e851d03349b9ea6cf9ea00110fd
atif1998 <malatif39@ee.ceme.edu.pk>
2023-05-08 16:12:15 -0800

    first commit

---
+++ pages/index.jsx
@@ -0,0 +1,25 @@
+import React,{useState} from "react";
+import "./index.css"
+
+function HomePage() {
+  const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+  const [likes, setLikes] = useState(0);
+
+  function handleClick() {
+    setLikes(likes + 1);
+  }
+
+  return (
+    <div>
+      <ul>
+        {names.map((name) => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+
+      <button onClick={handleClick}>Like ({likes})</button>
+    </div>
+  );
+}
+export default HomePage
---
+++ pages/post/[postid].jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+ const [postid] = () => {
+  return (
+    <div>[postid]</div>
+  )
+}
+export default postid
